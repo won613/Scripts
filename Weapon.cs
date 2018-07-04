bf1fe04498359ef407e79707e535cc180dfe6066
chaezun <chaw109@naver.com>
2018-07-04 18:00:06 +0900

    2018/7/4
    Jun

--- Weapon.cs
+++ Weapon.cs
@@ -1,48 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour {
+public class Weapon : MonoBehaviour
+{
 
     public bool isFiring;
     public BulletController bullet;
-    
+    public int BulletCount;
+
     public float timeBetweenShots;
     private float shotCounter;
+    public double reloadtime;
+    private DateTime startreloadtime;
+    private TimeSpan checkreloadtime;
+    //private float timecount;
+    private bool reloadcheck;
+
+    //private void discountreloadtime(float timecount)
+    //{
+    //    reloadtime -= timecount*Time.deltaTime;
+    //}
 
     public Transform firePoint;
-   
+
 
     public PlayerController player;
-    
-    
-	// Update is called once per frame
-	void Update () {
+
+    void Start()
+    {
+        reloadcheck = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         if (Input.GetMouseButtonDown(0))
-            isFiring = true;
+             isFiring = true;
+
 
         if (Input.GetMouseButtonUp(0))
             isFiring = false;
 
-        if (isFiring)
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            isFiring = false;
+            reloadcheck = true;
+            startreloadtime = System.DateTime.Now;
+        }
+
+        if (reloadcheck)
+        {
+            checkreloadtime = System.DateTime.Now - startreloadtime;
+        }
+
+        if (reloadtime <= (checkreloadtime.TotalSeconds%60)&&reloadcheck)
+        {
+            if (player.whatGun == 0)
+            {
+                BulletCount = 10;
+            }
+
+            if (player.whatGun == 1)
+            {
+                BulletCount = 30;
+            }
+            reloadcheck = false;
+        }
+
+        if (isFiring && BulletCount > 0)
         {
             shotCounter -= Time.deltaTime;
-            if(shotCounter <= 0)
+            if (shotCounter <= 0)
             {
                 shotCounter = timeBetweenShots;
-                
                 Instantiate(bullet.transform, firePoint.transform.position, firePoint.rotation);
-                               
+                BulletCount--;
             }
-        } else
+            if (BulletCount < 0)
+            {
+                BulletCount = 0;
+            }
+        }
+        else
         {
             shotCounter = 0;
         }
 
-
-
-	}
-
-    
+    }
 }
