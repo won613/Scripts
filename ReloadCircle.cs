d8e8aad3c0301d63ec2d67b37c331b55c6a16154
gywn0805 <gywn0805@gmail.com>
2018-07-25 16:55:19 +0900

    0725HJHJ

---
+++ ReloadCircle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReloadCircle : MonoBehaviour {
+
+	public Transform LoadingBar;
+	public Transform TextIndicator;
+	[SerializeField]
+	private float currentAmount;
+	[SerializeField]
+	private float speed;
+	public Weapon weapon;
+
+		
+	// Update is called once per frame
+	void Update () {
+		transform.position = Input.mousePosition;
+		if (currentAmount < 100)
+		{
+			currentAmount += speed * Time.deltaTime;
+			TextIndicator.GetComponent<Text>().text = ((int)currentAmount).ToString();
+
+		}
+		else
+		{
+			TextIndicator.GetComponent<Text>().text = "DONE!";
+		}
+		LoadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
+		
+	}
+}
